6a44649d554e545763bdcda88bb79efd0586dfc0
Rayen Bahroun <183955671+chkobbaa@users.noreply.github.com>
2025-07-10 03:34:40 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://football-backend-2un2.onrender.com/api', // change to your backend URL
-  API.post('/auth/signup', formData);
+  API.post('/auth/signup', formData),
 });
 
 API.interceptors.request.use((req) => {
